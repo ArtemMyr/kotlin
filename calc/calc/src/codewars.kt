6aa6b3ef2be751b8d590a88ff986d393a87388d0
ArtemMyr <ar.muravyov01@yandex.ru>
2024-01-18 11:10:54 +0300

    codewars

---
+++ calc/calc/src/codewars.kt
@@ -0,0 +1,29 @@
+fun main() {
+//Grasshopper - Summation
+//object GrassHopper {
+//    fun summation(n:Int):Int {
+//        var a = 0
+//        for (i in 1..n){
+//            a+=i
+//        }
+//        return a
+//    }
+//}
+
+//Total amount of points
+//    var games = listOf("1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3");
+//    var result = 0;
+//    for (i in games){
+//        var x = i[0]
+//        var y = i[2]
+//        if(x>y){
+//            result += 3
+//        }
+//
+//        if(x == y) {
+//            result += 1
+//        }
+//    }
+//    return result
+//    }
+}
